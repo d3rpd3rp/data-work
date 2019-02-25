e287f4805f37b2ee6c12571e0eb4cb2dfaa969b4
johnled <john_lednak@cysiv.com>
2019-02-25 00:56:08 -0500

    db, table, user, column headers all being created. onto inserting rows.

--- db-init-2.r
+++ db-init-2.r
@@ -14,36 +14,12 @@ library(DBI)
 #path<-arg1 
 
 con <- dbConnect(RMySQL::MySQL(), user='root', password='Spring2019!!', host='127.0.0.1')
-dbSendQuery(con, "CREATE DATABASE AONEDB;
-                  USE AONEDB;
-                  CREATE TABLE aonetable (id INT NOT NULL AUTO_INCREMENT, date DATE, symbol VARCHAR(20), \
-                                          open FLOAT, high FLOAT, low FLOAT, close FLOAT, PRIMARY KEY (id));
-                  GRANT ALL PRIVILEGES ON AONEDB.* TO 'root'@'127.0.0.1';"
-dbDisconnect(con)
-
-
-"""
-file.names <- dir(path, pattern =".csv")
-for(i in 1:length(file.names)){
-  samplerows <- read.csv(file.names[i], header=FALSE, sep=",")
-  str(samplerows)
-}
-
-file.names <- dir(path, pattern =".csv")
-for(i in 1:length(file.names)){
-    #idea from https://stackoverflow.com/questions/44288358/is-there-a-faster-way-to-upload-data-from-r-to-mysql
-    dbSendQuery(con, "LOAD DATA LOCAL INFILE file.names[i]
-                  INTO TABLE AONETABLE
-                  FIELDS TERMINATED by ','
-
-#path<-arg1 
-
-con <- dbConnect(RMySQL::MySQL(), user='root', password='Spring2019!!', host='127.0.0.1')
-dbSendQuery(con, "CREATE DATABASE AONEDB;
-                  USE AONEDB;
-                  CREATE TABLE aonetable (id INT NOT NULL AUTO_INCREMENT, date DATE, symbol VARCHAR(20), \
-                                          open FLOAT, high FLOAT, low FLOAT, close FLOAT, PRIMARY KEY (id));
-                  GRANT ALL PRIVILEGES ON AONEDB.* TO 'root'@'127.0.0.1';"
+dbSendQuery(con, "CREATE DATABASE AONEDB;")
+dbSendQuery(con, "USE AONEDB;")
+#credit for the id generation https://programminghistorian.org/en/lessons/getting-started-with-mysql-using-r#create-database
+dbSendQuery(con, "CREATE TABLE aonetable (id INT NOT NULL AUTO_INCREMENT, date DATE, symbol VARCHAR(20), \
+                                          open FLOAT, high FLOAT, low FLOAT, close FLOAT, PRIMARY KEY (id));")
+dbSendQuery(con, "GRANT ALL PRIVILEGES ON AONEDB.* TO 'mysqladmin'@'127.0.0.1' IDENTIFIED BY 'mysqladmin';")
 dbDisconnect(con)
 
 
@@ -71,4 +47,4 @@ con <- dbConnect(MySQL(),
   password = 'pw',
   host = 'amazonaws.com',
   dbname = 'db_name')
-  """
+"""
