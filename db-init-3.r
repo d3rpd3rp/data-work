b069df5890dfcb4ac9a3d06d9f524cd931a104bd
johnled <john_lednak@cysiv.com>
2019-02-25 02:13:11 -0500

    a progress. trying to write table from dataframe..

--- db-init-3.r
+++ db-init-3.r
@@ -1,4 +1,5 @@
- #r routine to init the db based on set of csv files
+  #!/usr/bin/Rscript
+  #r routine to init the db based on set of csv files
   #ex. Rscript foo.R arg1 arg2
   #! /path/to/Rscript
   #args <- commandArgs(TRUE)
@@ -10,26 +11,26 @@
 
   library(DBI)
 
-  path<-arg1
+  args<-commandArgs(TRUE)
+  path<-args[1]
 
   con <- dbConnect(RMySQL::MySQL(), user='root', password='Spring2019!!', host='127.0.0.1')
   dbSendQuery(con, "CREATE DATABASE AONEDB;")
   dbSendQuery(con, "USE AONEDB;")
   #credit for the id generation https://programminghistorian.org/en/lessons/getting-started-with-mysql-using-r#create-database
-  dbSendQuery(con, "CREATE TABLE aonetable (id INT NOT NULL AUTO_INCREMENT, date DATE, symbol VARCHAR(20), \
+  dbSendQuery(con, "CREATE TABLE aonetable (id INT NOT NULL AUTO_INCREMENT, date DATE, useless INTEGER, symbol VARCHAR(20), \
                                                         open FLOAT, high FLOAT, low FLOAT, close FLOAT, PRIMARY KEY (id));")
   dbSendQuery(con, "GRANT ALL PRIVILEGES ON AONEDB.* TO 'mysqladmin'@'127.0.0.1' IDENTIFIED BY 'mysqladmin';")
   dbDisconnect(con)
 
-  file.names <- dir(path, pattern =".csv")
+  file.names <- dir(path, pattern ="*.csv")
 
   con <- dbConnect(RMySQL::MySQL(), user='mysqladmin', password='mysqladmin', dbname='AONEDB', host='127.0.0.1')
   dbSendQuery(con, "USE AONEDB;")
 
   for(i in 1:length(file.names)){
               #idea from https://stackoverflow.com/questions/44288358/is-there-a-faster-way-to-upload-data-from-r-to-mysql
-              filepath <- paste(path, file.names[i])
-              dbSendQuery(con, "LOAD DATA LOCAL INFILE file.names[i] INTO TABLE aonetable FIELDS TERMINATED by ',' ENCLOSED BY '"' \
-                                LINES TERMINATED BY \n;")
-
+              dbSendQuery(con, "LOAD DATA LOCAL INFILE '", file.names[i], "' INTO TABLE aonetable FIELDS TERMINATED by ',' ENCLOSED BY '"' \
+                                LINES TERMINATED BY '\r\n';")
+  }
   dbDisconnect(con)
---
+++ db-init-csv-only.r
@@ -0,0 +1,21 @@
+library(DBI)
+
+args<-commandArgs(TRUE)
+path<-args[1]
+
+  
+file.names <- dir(path, pattern ="*.csv")
+
+con <- dbConnect(RMySQL::MySQL(), user='mysqladmin', password='mysqladmin', dbname='AONEDB', host='127.0.0.1')
+dbSendQuery(con, "USE AONEDB;")
+
+for(i in 1:length(file.names)){
+  filepath <- paste(path, file.names[i], sep="/")
+  samplerows <- read.csv(filepath, header=FALSE, sep=",")
+  print(typeof(samplerows))
+  print(typeof(str(samplerows))
+  #idea from https://stackoverflow.com/questions/44288358/is-there-a-faster-way-to-upload-data-from-r-to-mysql
+  dbWriteTable(con, name="aonetable", samplerows, append=TRUE )
+  }
+dbDisconnect(con)
+
